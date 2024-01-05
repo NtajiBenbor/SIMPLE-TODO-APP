0c70f567bd53b42a177e3544a49beb521569cb28
Ntaji Benedict I <benedictntaji@gmail.com>
2024-01-05 16:52:21 +0100

    updated js code to include index input validation; if the input is not a number it will output an errpr msg to the user

--- todo.js
+++ todo.js
@@ -5,7 +5,7 @@ const todo =['WASH CAR','BUY LAPTOP','LEARN TO CODE'];
 while(userInput !== 'quit' && userInput !== 'q'){
 
 
-    if(userInput === 'list'){
+    if(userInput === 'list'){ 
         console.log('*********');
         for(let i = 0; i < todo.length; i++){
             console.log(`${i}: ${todo[i].toUpperCase()}`);
@@ -18,10 +18,15 @@ while(userInput !== 'quit' && userInput !== 'q'){
          console.log(`${userInput.toUpperCase()} added to the list`);
     }
     else if(userInput === 'delete'){
-       userInput = prompt('what task do you want to delete?');
-       let removed = todo.splice(userInput,1);
+       userInput = parseInt(prompt('what task do you want to delete?'));
+       if (!Number.isNaN(userInput)) {
+        let removed = todo.splice(userInput,1);
         
         console.log(`${removed} Succesfully Deleted!`);
+       }else{
+        console.log('Invalid index');
+       }
+       
     }
 
     userInput = prompt('what do you want to do?');
